84208352c37296292e1b3437bcfcc8733c310ae1
Gleb <myemail@whatever.com>
2021-03-04 18:33:32 +0300

    Update lab and README

--- Lab1.py
+++ Lab1.py
@@ -1,3 +1,11 @@
 import logging
-logging.basicConfig(level=logging.INFO,format='%(message)s')
-logging.info('Lab1 message.')
+
+
+def fib(n):
+    if n < 2:
+        return n
+    return fib(n-1)+fib(n-2)
+
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+logging.info(f'Fibonacci number(10):{fib(10)}')
---
+++ README.txt
@@ -0,0 +1,4 @@
+* https://githowto.com/ru
+* https://help.ubuntu.ru/wiki/командная_строка
+* https://docs.docker.com
+* https://hub.docker.com/repository/docker/keytus/lab1
